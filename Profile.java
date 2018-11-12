00ab7f0ca2f24fc164a894e99fbabb0cf875de09
vedant781999 <40128557+vedant781999@users.noreply.github.com>
2018-11-12 20:05:19 +0530

    Update Profile.java

--- Profile.java
+++ Profile.java
@@ -1,4 +1,4 @@
-package ashish;
+package brivago;
 
 import javax.swing.*;
 import javax.imageio.*;
@@ -22,31 +22,38 @@ class Profile
 		frame.setContentPane(new JLabel(new ImageIcon("beach.jpg")));
 		frame.setLayout(null);
 
-		ImageIcon brivagoicon = new ImageIcon("brivagoicon.jpg");//settin app icon
+		ImageIcon brivagoicon = new ImageIcon("brivagoicon.jpg");//setting app icon
 		frame.setIconImage(brivagoicon.getImage());
 	
-		JButton profile = new JButton("profile");//profile button setup
-		profile.setBounds(1405,10,77,30);
-		frame.getContentPane().add(profile);
+		JButton mybook = new JButton("My Bookings");//profile button setup
+		mybook.setBounds(1365,10,108,30);
+		mybook.addActionListener(new BookingListener());
+		frame.getContentPane().add(mybook);
 
 		JButton logout = new JButton();//logout button setup
 		logout.setBounds(1495, 10, 30,30);
+		logout.addActionListener(new LogoutListener());
 		frame.getContentPane().add(logout);
 		ImageIcon image = new ImageIcon("power.jpg");
 		logout.setIcon(image);
-
-		JButton mybook = new JButton("My Bookings");//mybooking button setup
-		mybook.setBounds(1285,10, 108, 30);
-		frame.getContentPane().add(mybook);
+		
+		JButton home = new JButton("Home");//profile button setup
+		home.addActionListener(new HomeListener());
+		home.setBounds(10,10,80,32);
+		frame.getContentPane().add(home);		
 		
 		JLabel heading =new JLabel("PROFILE");
 		heading.setBounds(500,100,300,50);
-		Font font = new Font("Courier",Font.BOLD,25);
-		heading.setFont(font);
+		Font font1 = new Font("Courier",Font.BOLD,25);
+		heading.setFont(font1);
+		heading.setForeground(Color.RED);
 		frame.getContentPane().add(heading);
 		
+		Font font2 = new Font("serif",Font.BOLD,20);
+		
 		JLabel nameLabel = new JLabel("Name");
-		nameLabel.setBounds(400,200,108,30);
+		nameLabel.setBounds(360,200,158,30);
+		nameLabel.setFont(font2);
 		frame.getContentPane().add(nameLabel);
 		
 		JTextField name = new JTextField(1000);
@@ -55,7 +62,8 @@ class Profile
 		name.setText("Ashish");
 		
 		JLabel dateOfBirthLabel = new JLabel("Date of Birth");
-		dateOfBirthLabel.setBounds(400,270,108,30);
+		dateOfBirthLabel.setBounds(360,270,158,30);
+		dateOfBirthLabel.setFont(font2);
 		frame.getContentPane().add(dateOfBirthLabel);
 		
 		JTextField dateOfBirth = new JTextField(1000);
@@ -64,9 +72,9 @@ class Profile
 		dateOfBirth.setText("02/01/1999");
 		
 		JLabel residentialAddressLabel = new JLabel("Residential Address");
-		residentialAddressLabel.setBounds(400,340,130,30);
+		residentialAddressLabel.setBounds(360,340,240,30);
+		residentialAddressLabel.setFont(font2);
 		frame.getContentPane().add(residentialAddressLabel);
-		
 		
 		JTextField residentialAddress = new JTextField(1000);
 		residentialAddress.setBounds(600,340,300,30);
@@ -74,16 +82,18 @@ class Profile
 		residentialAddress.setText("Address");
 		
 		JLabel emailIdLabel = new JLabel("Email ID ");
-		emailIdLabel.setBounds(400,410,108,30);
-		
+		emailIdLabel.setBounds(360,410,158,30);
+		emailIdLabel.setFont(font2);
 		frame.getContentPane().add(emailIdLabel);
+		
 		JTextField emailId = new JTextField(1000);
 		emailId.setBounds(600,410,300,30);
 		frame.getContentPane().add(emailId);
 		emailId.setText("example@example.com");
 		
 		JLabel userNameLabel = new JLabel("User Name");
-		userNameLabel.setBounds(400,480,108,30);
+		userNameLabel.setBounds(360,480,158,30);
+		userNameLabel.setFont(font2);
 		frame.getContentPane().add(userNameLabel);
 		
 		JTextField userName = new JTextField(1000);
