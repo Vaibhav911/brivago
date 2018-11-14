0ca82035768d03768b21199484cd3184ae26f427
vedant781999 <40128557+vedant781999@users.noreply.github.com>
2018-11-14 21:36:54 +0530

    Update Registration.java

--- Registration.java
+++ Registration.java
@@ -1,10 +1,15 @@
-package ashish;
+package brivago;
 
 import javax.swing.*;
 import javax.imageio.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.*;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Random;
 
 class Registration
 {
@@ -26,20 +31,6 @@ class Registration
 		Font font2 = new Font("Courier",Font.BOLD,16);
 		ImageIcon brivagoicon = new ImageIcon("brivagoicon.jpg");//settin app icon
 		frame.setIconImage(brivagoicon.getImage());
-	
-		JButton profile = new JButton("profile");//profile button setup
-		profile.setBounds(1405,10,77,30);
-		frame.getContentPane().add(profile);
-
-		JButton logout = new JButton();//logout button setup
-		logout.setBounds(1495, 10, 30,30); 
-		frame.getContentPane().add(logout);
-		ImageIcon image = new ImageIcon("power.jpg");
-		logout.setIcon(image);
-
-		JButton mybook = new JButton("My Bookings");//mybooking button setup
-		mybook.setBounds(1285,10, 108, 30);
-		frame.getContentPane().add(mybook);
 		
 		JLabel heading =new JLabel("Register on Brivago");
 		heading.setBounds(400,100,300,50);
@@ -117,8 +108,101 @@ class Registration
 		frame.getContentPane().add(confirmPassword);
 		
 		JButton register = new JButton("Register");
-		register.setBounds(740,700,100,50);
+		register.setBounds(500,700,100,50);
 		frame.getContentPane().add(register);
+		
+		JButton advertisement= new JButton() ;
+		advertisement.setBounds(1135,100,400,700);
+		frame.getContentPane().add(advertisement);
+		
+		Random random = new Random();
+	      int randomInteger = random.nextInt(5)+1;
+	      System.out.println(randomInteger);
+	      
+	      String link = null;
+	      switch(randomInteger)
+	      {
+	          case 1:
+	        	  ImageIcon image1=new  ImageIcon("add1.png"); 
+	              advertisement.setIcon(image1);
+	              link ="https://www.firstpost.com/shows/9-months/9-months-season-3-toddler-schooling-part-2-5394261.html";
+	              break;
+	          case 2:
+	        	  ImageIcon image2=new  ImageIcon("add2.png"); 
+	              advertisement.setIcon(image2);
+	              link =" https://www.upwardly.in/en/sip-calculator?utm_source=Display&utm_medium=adwordsbanner&utm_campaign=SipCalculatorDisplayInMkt&gclid=EAIaIQobChMIwMjMi_3L3gIVhRZoCh3gQwxrEAEYASAAEgJ-8_D_BwE";
+	              break;
+	          case 3:
+	        	  ImageIcon image3=new  ImageIcon("add3.png"); 
+	              advertisement.setIcon(image3);
+	              link ="https://practice.geeksforgeeks.org/courses/fork-cpp";
+	              break;
+	          case 4:
+	        	  ImageIcon image4=new  ImageIcon("add4.png"); 
+	              advertisement.setIcon(image4);
+	              link ="https://www.amazon.in/Kindle-Paperwhite-Resolution-Display-Built/dp/B00QJDOEAO?_encoding=UTF8&*Version*=1&*entries*=0&portal-device-attributes=desktop&ref_=kindle_ams_gw_pc";
+	              break;
+	          case 5:
+	        	  ImageIcon image5=new  ImageIcon("add5.png"); 
+	              advertisement.setIcon(image5);
+	              link =" http://events.computing.co.uk/itlfskytap";
+	              break;
+		
+	     }
+	      
+        final String finalLink = link;
+	      
+	      advertisement.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e){
+					try {
+						openWebpage(finalLink);
+					}
+					catch(Exception se)
+					{
+						se.printStackTrace();
+					}
+				}
+				public void openWebpage(String urlString) {
+				     try {
+				       Desktop.getDesktop().browse(new URL(urlString).toURI());
+				    	  } catch (Exception e) {
+				    	        e.printStackTrace();
+				    	    }
+				  }
+				
+			});
+
+
+		
+		register.addActionListener(new ActionListener()
+		{
+		    public void actionPerformed(ActionEvent ae) 
+		    {
+		    	String s1 = name.getText();
+		    	String s2 = dateOfBirth.getText();
+		    	String s3 = residentialAddress.getText();
+		    	String s4 = emailId.getText();
+		    	String s5 = userName.getText();
+		    	char[] s6 = password.getPassword();
+		    	char[] s7 = confirmPassword.getPassword();
+		    	
+		    	for(int i=0;i<s6.length;i++)
+		    	{
+		    		if(!(Arrays.equals(password.getPassword(), confirmPassword.getPassword())))
+		    	   {
+		    			
+		    			
+		    			JLabel ha = new JLabel("Re-enter password");
+		    			ha.setBounds(300,600,100,50);
+		    			frame.add(ha);
+		    			
+		    		}
+		    	}
+		    	
+		      
+			}
+		    
+		 });
 		
     frame.revalidate();
 		frame.repaint(); 
