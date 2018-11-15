7e1ed765859bf8f79e150dd8327167cde83ad3c0
vedant781999 <40128557+vedant781999@users.noreply.github.com>
2018-11-15 10:30:13 +0530

    Update MyBooking.java

--- MyBooking.java
+++ MyBooking.java
@@ -1,63 +1,86 @@
+package brivago;
 import java.awt.*;
+import java.io.IOException;
 import javax.swing.*;
 public class MyBooking 
 {
-	MyBooking()
+	public static void main(String args[]) throws IOException
 	{
-		JFrame jf=new JFrame("Brivago");
-		jf.setSize(1920,1080);
-		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jf.setVisible(true);
-		jf.setContentPane(new JLabel(new ImageIcon("beach.jpg")));
-		jf.setLayout(null);
+	    MyBooking booking =new MyBooking();
+		booking.gui();
+	}
+	
+	public void gui() throws IOException
+	{
+		JFrame frame=new JFrame("My Bookings");
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		frame.setSize(1920,1080);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setContentPane(new JLabel(new ImageIcon("beach.jpg")));
+		frame.setLayout(null);
 		//Adding Image Icon to the template
 		ImageIcon brivagoicon = new ImageIcon("brivagoicon.jpg");
-		jf.setIconImage(brivagoicon.getImage());
+		frame.setIconImage(brivagoicon.getImage());
+		
+		JButton profile = new JButton("Profile");//profile button setup
+		profile.addActionListener(new ProfileListener());
+		profile.setBounds(1405,10,77,30);
+		frame.getContentPane().add(profile);
+
+		JButton logout = new JButton();//logout button setup
+		logout.setBounds(1495, 10, 30,30);
+		logout.addActionListener(new LogoutListener());
+		frame.getContentPane().add(logout);
+		ImageIcon image = new ImageIcon("power.jpg");
+		logout.setIcon(image);
+		
+		JButton home = new JButton("Home");//profile button setup
+		home.addActionListener(new HomeListener());
+		home.setBounds(10,10,80,32);
+		frame.getContentPane().add(home);		
 		
 		//Making two fonts for heading and body respectively
         Font font1=new Font("Verdana",Font.BOLD,24);
         Font font2=new Font("serif",Font.BOLD,18);
         
         //Making five labels for their respective fields 
-		JLabel jl1=new JLabel("Date");
-		jl1.setBounds(500,150,300,150);
-		jl1.setFont(font2);
-		jf.add(jl1);
+		JLabel dateLabel=new JLabel("Date");
+		dateLabel.setBounds(500,150,300,150);
+		dateLabel.setFont(font2);
+		frame.add(dateLabel);
 		
-		JLabel jl2=new JLabel("Day");
-		jl2.setBounds(500,200,300,150);
-		jl2.setFont(font2);
-		jf.add(jl2);
+		JLabel dayLabel=new JLabel("Day");
+		dayLabel.setBounds(500,200,300,150);
+		dayLabel.setFont(font2);
+		frame.add(dayLabel);
 
-		JLabel jl3=new JLabel("Hotel");
-		jl3.setBounds(170,150,300,150);
-		jl3.setFont(font2);
-		jf.add(jl3);
+		JLabel hotelLabel=new JLabel("Hotel");
+		hotelLabel.setBounds(170,150,300,150);
+		hotelLabel.setFont(font2);
+		frame.add(hotelLabel);
 
-		JLabel jl4=new JLabel("City");
-		jl4.setBounds(170,200,300,150);
-		jl4.setFont(font2);
-		jf.add(jl4);
+		JLabel cityLabel=new JLabel("City");
+		cityLabel.setBounds(170,200,300,150);
+		cityLabel.setFont(font2);
+		frame.add(cityLabel);
 
-		JLabel jl5=new JLabel("My Bookings");
-		jl5.setBounds(170,30,300,150);
-		jl5.setFont(font1);
-		jf.add(jl5);
+		JLabel myBookingsLabel=new JLabel("My Bookings");
+		myBookingsLabel.setBounds(170,30,300,150);
+		myBookingsLabel.setFont(font1);
+		frame.add(myBookingsLabel);
 		
 		//Making 2 buttons to view and cancel booking
-		JButton jb1=new JButton("View");
-		jb1.setBounds(800,265,120,30);
-		jf.add(jb1);
+		JButton viewButton=new JButton("View");
+		viewButton.setBounds(800,265,120,30);
+		frame.add(viewButton);
 		
-		JButton jb2=new JButton("Cancel");
-		jb2.setBounds(920,265,120,30);
-		jf.add(jb2);
-	
-		jf.revalidate();
-		jf.repaint();	
+		JButton cancelButton=new JButton("Cancel");
+		cancelButton.setBounds(920,265,120,30);
+		frame.add(cancelButton);
+	    
+		
+		frame.revalidate();
+		frame.repaint();	
 	}	
-	public static void main(String[] args)
-	{
-		SwingUtilities.invokeLater(new Runnable() {public void run() {new MyBooking();}});	
-	}
 }
